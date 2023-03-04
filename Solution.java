59977f207a0ffbd06fb2a224a4374045c8a0cc37
SOURAV MANDAL <sourav0200@gmail.com>
2023-03-04 19:40:56 +0600

    Add two array

---
+++ Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            sum %= 10;
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
